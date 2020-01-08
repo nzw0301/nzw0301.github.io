6b042819b0deb97e16266b152913f9ef60c156a2
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2020-01-08 23:58:21 +0900

    Update photos

--- assets/js/random_image.js
+++ assets/js/random_image.js
@@ -2,15 +2,16 @@
 
 var imageURLs = [
     "https://www.dropbox.com/s/feoh20rk00zuevm/nzw0.jpg?raw=1",
-    "https://www.dropbox.com/s/rqq6kyw5hijdo86/nzw1.png?raw=1"
+    "https://www.dropbox.com/s/rqq6kyw5hijdo86/nzw1.png?raw=1",
+    "https://www.dropbox.com/s/zx3ad4pewvqg6mq/nzw2.png?raw=1"
 ];
 function getImageTag() {
     var img = '<img src=\"';
     var randomIndex = Math.floor(Math.random() * imageURLs.length);
     img += imageURLs[randomIndex];
-    img += '\" alt=\"nzw picture\"'; 
-    img += 'height=\"240\" width=\"160\" />'; 
-    
-    
+    img += '\" alt=\"nzw picture\"';
+    img += 'height=\"500\" width=\"500\" />';
+
+
     return img;
 }
