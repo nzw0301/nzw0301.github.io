03a5c7b4fc3197afc837119604d5e3158dd71613
Kento Nozawa <nzw0301@preferred.jp>
2025-12-16 22:38:59 +0900

    ?

--- _utils/generate_publications.py
+++ _utils/generate_publications.py
@@ -122,7 +122,7 @@ with open(path) as bibtex_file:
             selected_list.append(line)
 
 with open("../_includes/publication.md", "w") as f:
-    f.write("## Selected journal / Conference papers\n\n")
+    f.write("## Selected journal and conference papers\n\n")
     f.write("\n".join(selected_list))
     f.write("\n")
     f.write("\n")
